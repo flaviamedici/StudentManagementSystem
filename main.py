342068e24d1d5fa38dbaf12153e12337c24327ef
flaviamedici <flaviaomedici@gmail.com>
2026-05-09 11:17:47 -0700

    Application to calculate age using PyQt6 Guis

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+
+from PyQt6.QtWidgets import (QApplication, QVBoxLayout, QLabel, QWidget,
+                             QGridLayout, QLineEdit, QPushButton)
+from datetime import datetime
+
+
+class AgeCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        grid = QGridLayout()
+
+        name_label = QLabel("Name: ")
+        name_line_edit = QLineEdit()
+
+        date_birth_label = QLabel("Birth Date: ")
+        self.date_birth_line_edit = QLineEdit()
+
+        calculate_button = QPushButton("Calculate")
+        calculate_button.clicked.connect(self.calculate_age)
+        output_label = QLabel("")
+
+
+        grid.addWidget(name_label, 0, 0)
+        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(date_birth_label, 1, 0)
+        grid.addWidget(self.date_birth_line_edit, 1, 1)
+        grid.addWidget(calculate_button, 2, 0, 1, 2)
+        grid.addWidget(output_label, 3, 0, 1, 2)
+
+        self.setLayout(grid)
+
+def calculate_age(self):
+    current_year = datetime.now().year
+    year_of_birth = self.date_birth_line_edit.text()
+
+    age = current_year - year_of_birth
+    self.output_label.setText("Age: " + str(age))
+    return age
+
+app = QApplication(sys.argv)
+age_calculator = AgeCalculator()
+age_calculator.show()
+sys.exit(app.exec())
