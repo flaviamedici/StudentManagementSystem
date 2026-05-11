734118d28dccd347a90a31b7d5fa3c2a59518d2d
flaviamedici <flaviaomedici@gmail.com>
2026-05-11 08:31:40 -0700

    Updating code

--- main.py
+++ main.py
@@ -8,39 +8,37 @@ from datetime import datetime
 class AgeCalculator(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Age Calculator")
         grid = QGridLayout()
 
         name_label = QLabel("Name: ")
-        name_line_edit = QLineEdit()
+        self.name_line_edit = QLineEdit()
 
         date_birth_label = QLabel("Birth Date: ")
         self.date_birth_line_edit = QLineEdit()
 
         calculate_button = QPushButton("Calculate")
         calculate_button.clicked.connect(self.calculate_age)
-        output_label = QLabel("")
+        self.output_label = QLabel("")
 
 
         grid.addWidget(name_label, 0, 0)
-        grid.addWidget(name_line_edit, 0, 1)
+        grid.addWidget(self.name_line_edit, 0, 1)
         grid.addWidget(date_birth_label, 1, 0)
         grid.addWidget(self.date_birth_line_edit, 1, 1)
         grid.addWidget(calculate_button, 2, 0, 1, 2)
-        grid.addWidget(output_label, 3, 0, 1, 2)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
 
 
         self.setLayout(grid)
 
-
-#function for calculating the age
-
     def calculate_age(self):
         current_year = datetime.now().year
         date_of_birth = self.date_birth_line_edit.text()
-        year_of_birth = datetime.strftime(date_of_birth, "%m/%d/%Y").date().year
+        year_of_birth = datetime.strptime(date_of_birth, "%m/%d/%Y").date().year
         age = current_year - year_of_birth
         self.output_label.setText("Age: " + str(age))
-        return age
+
 
 app = QApplication(sys.argv)
 age_calculator = AgeCalculator()
